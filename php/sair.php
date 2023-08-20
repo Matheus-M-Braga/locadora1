7a1de82a6190f51c0e7b93f5d3bbfd33c05ffaa4
Telanium <medeirosmatheus50677@gmail.com>
2023-08-20 17:49:50 -0300

    alert - log in/out

--- php/sair.php
+++ php/sair.php
@@ -1,6 +1,26 @@
-<?php
-    session_start();
-    unset($_SESSION['email']);
-    unset($_SESSION['senha']);
-    header('Location: ../index.php');
-?>
+<!DOCTYPE html>
+
+<head>
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <link rel="stylesheet" href="../css/style.css?<?php echo rand(1, 1000); ?>" media="all">
+   <link rel="stylesheet" href="../css/mediaquery.css?<?php echo rand(1, 1000); ?>">
+</head>
+
+<body>
+   <?php
+   session_start();
+   unset($_SESSION['email']);
+   unset($_SESSION['senha']);
+   echo "
+   <script>
+      Swal.fire({
+         title: 'Usuário desconectado!',
+         text: '',
+         icon: 'success',
+         showConfirmButton: false,
+         timer: 1500
+      })
+      .then(() => {window.location.href = '../index.php';})
+   </script>";
+   ?>
+</body>
--- php/testelogin.php
+++ php/testelogin.php
@@ -1,6 +1,15 @@
-<?php
+<!DOCTYPE html>
+
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="../css/style.css?<?php echo rand(1, 1000); ?>" media="all">
+    <link rel="stylesheet" href="../css/mediaquery.css?<?php echo rand(1, 1000); ?>">
+</head>
+
+<body>
+    <?php
     session_start();
-    if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])){
+    if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
 
         include_once('config.php');
         $email = $_POST['email'];
@@ -8,21 +17,50 @@
 
         $sql = "SELECT * FROM gerenciadores WHERE usuario = '$email' and senha = '$senha'";
 
-        $result = $conexao -> query($sql);
+        $result = $conexao->query($sql);
 
 
-        if(mysqli_num_rows($result) < 1){
+        if (mysqli_num_rows($result) < 1) {
             unset($_SESSION['email']);
             unset($_SESSION['senha']);
-            header('Location: index.php');
-        }
-        else{
+            echo "
+            <script>
+                Swal.fire({
+                    title: 'Nenhum registro encontrado!',
+                    text: '',
+                    icon: 'error',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                .then(() => {window.location.href = '../index.php';})
+            </script>";
+        } else {
             $_SESSION['email'] = $email;
             $_SESSION['senha'] = $senha;
-            header('Location: ../inicio.php ');
+            echo "
+            <script>
+                Swal.fire({
+                    title: 'Usuário logado!',
+                    text: '',
+                    icon: 'success',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                .then(() => {window.location.href = '../inicio.php';})
+            </script>";
         }
+    } else {
+        echo "
+        <script>
+            Swal.fire({
+                title: 'Nenhum registro encontrado!',
+                text: '',
+                icon: 'error',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            .then(() => {window.location.href = '../index.php';})
+        </script>";
     }
-    else{
-        header('Location: ../index.php');
-    }
-?>
+    ?>
+</body>
