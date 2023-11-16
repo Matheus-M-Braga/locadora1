b0e4224e8ba80a4da24613c9711ecc58c5acfee6
Telanium <medeirosmatheus50677@gmail.com>
2023-11-16 00:32:35 -0300

    .

--- ...versaoWEB/php/getregistersdata.php
+++ ...versaoWEB/php/getregistersdata.php
@@ -1,69 +1,67 @@
-<?php 
+<?php
 include_once('config.php');
 
 $data = array();
 
 // Usuários
 $sqlUser = "SELECT * FROM usuarios";
-$resultUser = $conexao -> query($sqlUser);
+$resultUser = $conexao->query($sqlUser);
 
-if($resultUser -> num_rows > 0){
+if ($resultUser->num_rows > 0) {
    $usuarios = array();
-   while ($row = $resultUser -> fetch_assoc()) {
+   while ($row = $resultUser->fetch_assoc()) {
       $id = $row['id']; // Obtém o ID do registro
       $usuarios[$id] = $row; // Usar o ID como índice
    }
-   $data['user'] = $usuarios;
-} 
+   $data['User'] = $usuarios;
+} else {
+   $data['User'] = array();
+}
 
 // Livros
 $sql = "SELECT * FROM livros";
-$result = $conexao -> query($sql);
+$result = $conexao->query($sql);
 
-if($result -> num_rows > 0){
+if ($result->num_rows > 0) {
    $livros = array();
-   while ($row = $result -> fetch_assoc()) {
+   while ($row = $result->fetch_assoc()) {
       $id = $row['id']; // Obtém o ID do registro
       $livros[$id] = $row; // Usar o ID como índice
    }
-   $data['livro'] = $livros;
-} 
-else{
-   $data['livro'] = array();
+   $data['Book'] = $livros;
+} else {
+   $data['Book'] = array();
 }
 
 // Editoras
 $sql = "SELECT * FROM editoras";
-$result = $conexao -> query($sql);
+$result = $conexao->query($sql);
 
-if($result -> num_rows > 0){
+if ($result->num_rows > 0) {
    $editoras = array();
-   while ($row = $result -> fetch_assoc()) {
+   while ($row = $result->fetch_assoc()) {
       $id = $row['id']; // Obtém o ID do registro
       $editoras[$id] = $row; // Usar o ID como índice
    }
-   $data['editora'] = $editoras;
-} 
-else{
-   $data['editora'] = array();
+   $data['Publisher'] = $editoras;
+} else {
+   $data['Publisher'] = array();
 }
 
 // Aluguéis
 $sql = "SELECT * FROM alugueis";
-$resultAlug = $conexao -> query($sql);
+$resultAlug = $conexao->query($sql);
 
-if($result -> num_rows > 0){
+if ($result->num_rows > 0) {
    $alugueis = array();
-   while ($row = $resultAlug -> fetch_assoc()) {
+   while ($row = $resultAlug->fetch_assoc()) {
       $id = $row['id']; // Obtém o ID do registro
       $alugueis[$id] = $row; // Usar o ID como índice
    }
-   $data['aluguel'] = $alugueis;
-} 
-else{
-   $data['aluguel'] = array();
+   $data['Rental'] = $alugueis;
+} else {
+   $data['Rental'] = array();
 }
 
-// Armazena todas as consultas
+// Envia todos os dados em formato json
 echo json_encode($data);
-?>
