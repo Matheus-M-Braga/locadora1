070be733e47c28d62316e67692dd3365e441e923
Telanium <medeirosmatheus50677@gmail.com>
2023-11-16 00:35:53 -0300

    BranchWeb

--- ...versaoWEB/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUsername= 'id20793781_root';
-    $dbPassword = 'Admin123+';
-    $dbName = 'id20793781_locadorabd';
-
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-?>
